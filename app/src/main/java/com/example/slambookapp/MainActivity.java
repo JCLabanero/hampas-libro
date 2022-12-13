dbd9abdef6978d56438a2f6668aebc04198cb273
John Carlo <johncarlolabanero@gmail.com>
2022-12-14 01:01:22 +0800

    add intent on mainActivity

--- app/src/main/java/com/example/slambookapp/MainActivity.java
+++ app/src/main/java/com/example/slambookapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.slambookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,26 +10,30 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonLogin;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         View layout = findViewById(R.id.linearLayout3);
-
         EditText username = findViewById(R.id.editTextEmailAddress);
         EditText password = findViewById(R.id.editTextPassword);
-
-        Button buttonLogin = findViewById(R.id.buttonLogin);
-
-
-
+        buttonLogin = findViewById(R.id.buttonLogin);
+        /*        buttonLogin.setOnClickListener(tapListener);*/
         buttonLogin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
-//                Intent intent = new Intent(this, HomeActivity.class);
-//                startActivity(intent);
+            public void onClick(View v) {
+                Intent i = new Intent(context, HomeActivity.class);
+                startActivity(i);
             }
         });
     }
+    /*    View.OnClickListener tapListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+    Intent i = new Intent(context, HomeActivity.class);
+    startActivity(i);
+    }
+    };*/
 }
