a5fe31dd64bcb35b92feef4e35a0735e47377ee1
John Carlo <johncarlolabanero@gmail.com>
2022-12-14 11:24:39 +0800

    add another intent for regiter

--- app/src/main/java/com/example/slambookapp/MainActivity.java
+++ app/src/main/java/com/example/slambookapp/MainActivity.java
@@ -8,26 +8,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonLogin;
-    Context context = this;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Context context = this;
+
         View layout = findViewById(R.id.linearLayout3);
         EditText username = findViewById(R.id.editTextEmailAddress);
         EditText password = findViewById(R.id.editTextPassword);
-        buttonLogin = findViewById(R.id.buttonLogin);
-        /*        buttonLogin.setOnClickListener(tapListener);*/
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(context, HomeActivity.class);
-                startActivity(i);
+        Button buttonLogin = findViewById(R.id.buttonLogin);
+        TextView buttonSignUp = findViewById(R.id.textViewSignUp);
+
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, HomeActivity.class);
+                startActivity(intent);
             }
         });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(context, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        /*        buttonLogin.setOnClickListener(tapListener);*/
     }
     /*    View.OnClickListener tapListener = new View.OnClickListener() {
     @Override
