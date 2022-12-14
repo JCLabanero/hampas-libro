fedf475987759d1909fa7a49fedb26803b5dfdb2
John Carlo <johncarlolabanero@gmail.com>
2022-12-14 13:19:52 +0800

    add recyclerview content and row slam

---
+++ app/src/main/java/com/example/slambookapp/Content.java
@@ -0,0 +1,34 @@
+package com.example.slambookapp;
+
+public class Content {
+    private int image;
+    private String title;
+    private String details;
+
+    public Content(int image, String title, String details){
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
--- app/src/main/java/com/example/slambookapp/HomeActivity.java
+++ app/src/main/java/com/example/slambookapp/HomeActivity.java
@@ -1,17 +1,31 @@
 package com.example.slambookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    RecyclerView recyclerView1;
+    Button buttonAdd;
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        init();
+    }
 
+
+
+    public void init() {
+        recyclerView1 = findViewById(R.id.recycler);
+        buttonAdd = findViewById(R.id.buttonAdd);
     }
 }
