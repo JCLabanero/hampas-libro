4d1c72baa69f17f4d0d0a3ce9597115e59a16d3c
John Carlo <johncarlolabanero@gmail.com>
2022-12-18 01:26:10 +0800

    add question activity and edit answers activity

--- app/src/main/java/com/example/slambookapp/activities/AnswersActivity.java
+++ app/src/main/java/com/example/slambookapp/activities/AnswersActivity.java
@@ -29,7 +29,7 @@ public class AnswersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_answers);
         init();
     }
 
---
+++ app/src/main/java/com/example/slambookapp/activities/QuestionActivity.java
@@ -0,0 +1,15 @@
+package com.example.slambookapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.slambookapp.R;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+    }
+}
