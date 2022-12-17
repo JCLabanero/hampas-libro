e2b909445d3ac263aa2235dfba18e62011e1aedd
John Carlo <johncarlolabanero@gmail.com>
2022-12-17 23:05:00 +0800

    edit mainactivity to loginactivity and add register activity

--- app/src/main/java/com/example/slambookapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/slambookapp/activities/LoginActivity.java
@@ -12,11 +12,11 @@ import android.widget.TextView;
 
 import com.example.slambookapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Context context = this;
 
